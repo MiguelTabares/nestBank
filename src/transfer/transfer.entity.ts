1052d842b32fb5a4085b82ec3fa339f59b040991
Muten Roshi <144490177+Misttes@users.noreply.github.com>
2024-04-23 13:31:03 -0500

    Create transfer.entity.ts
    Entity

---
+++ src/transfer/transfer.entity.ts
@@ -0,0 +1,22 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose';
+
+@Schema( {timestamps: true} )
+export class Transfer extends Document{
+    @Prop({ require: true })
+    transferId: string;
+
+    @Prop({ require: true })
+    sender: string;
+
+    @Prop({ require: true })
+    receiver: string;
+
+    @Prop({ require: true })
+    amount: number;
+
+    @Prop({ require: true })
+    date: Date;
+}
+
+export const TransferSchema = SchemaFactory.createForClass(Transfer);
